28a3354a75da1471a174a024e3dc97ff4d51b2e2
David Martín Lozano <david.martin.lozano@gmail.com>
2017-07-18 15:46:20 +0200

    Added environments

--- src/app/cv/cv.service.ts
+++ src/app/cv/cv.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
 import { environment } from '../../environments/environment';
+import 'rxjs/add/operator/map'
 
 @Injectable()
-export class CVService {
+export class CVService{
 
   constructor(private http:Http)  {}
 
   //Method to access to back with API Rest. Return the response
   getCV(id): Observable<any> {
-    return this.http.get('/cv/'+id).map(response => response.json());
+    return this.http.get(environment.baseUrl+'/cv/'+id).map(response => response.json());
   }
 }
